38abf6bf832d75c191521516d3eca123f393c78c
watchmedash <mojazer2017@gmail.com>
2024-08-01 12:11:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const itemList = [
     "The Perks of Working in the Black Magic Industry",
     "Solo Leveling",
+    "Solo Leveling Ragnarok",
     "Dandadan",
     "Easy Survival in Another World",
     "The Player That Cant Level Up",
